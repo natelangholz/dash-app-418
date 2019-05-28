ee7d4d519fd9550c4ea4228baae5c4ed94e1a5f2
natelangholz <nathan.langholz@redbull.com>
2019-05-28 14:55:08 -0700

    dash tutorials

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# dash-app-418
+# Dash App 418
+
+This repo will be used as a self contained repo to create a Dash app that is deployed through Docker and then on an AMI. 
+
---
+++ dash-iris/app.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash import Dash
+from dash.dependencies import Input, Output
+
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import numpy as np
+import pandas as pd
+
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+app = Dash(__name__)
+server = app.server
+app.title='Iris Data Dashboard'
+
+# do all the machine learning
+iris = datasets.load_iris()
+colors = ["#E41A1C", "#377EB8", "#4DAF4A", \
+          "#984EA3", "#FF7F00", "#FFFF33", \
+          "#A65628", "#F781BF", "#999999"]
+number_of_clusters = range(6)
+
+
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['target'] = iris.target
+col = ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
+X = df[col]
+y = df["target"]
+
+df_list = []
+# declaring our algorithm and its parameters
+for n in number_of_clusters:
+    kmeans = KMeans(n_clusters=n + 1)
+    
+    # fit data to features
+    kmeans.fit(X)
+    
+    results_df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    results_df["predicted_classes"] = kmeans.labels_
+    
+    num_of_clusters = results_df['predicted_classes'].nunique()
+    df_list.append({
+                   'results_df': results_df,
+                   'num_of_clusters': num_of_clusters
+                   })
+
+dropdown_val = iris.feature_names
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+                      # Header
+                      [html.Tr([html.Th(col) for col in dataframe.columns])] +
+                      
+                      # Body
+                      [html.Tr([
+                                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+                                ]) for i in range(min(len(dataframe), max_rows))]
+                      )
+
+
+
+app.layout = html.Div(className="container", children=[
+                                                       html.Div(className="jumbotron text-center", children=[
+                                                                                                             html.H1("Iris Clustering"),
+                                                                                                             html.P("Select the X and Y to visualize the data using chosen number of clusters"),
+                                                                                                             html.P("A simple classification algorithm using K-means")
+                                                                                                             ]),
+                                                       dcc.Dropdown(className="col-md-4", id="dropdown_x",
+                                                                    options=[
+                                                                             {'label': val, 'value': val} for val in dropdown_val
+                                                                             ],
+                                                                    value=dropdown_val[0] #default value for first launch
+                                                                    ),
+                                                       dcc.Dropdown(className="col-md-4", id="dropdown_y",
+                                                                    options=[
+                                                                             {'label': val, 'value': val} for val in dropdown_val
+                                                                             ],
+                                                                    value=dropdown_val[1]
+                                                                    ),
+                                                       dcc.Dropdown(className="col-md-4", id="dropdown_k",
+                                                                    options=[
+                                                                             {'label': str(val+1), 'value': val} for val in number_of_clusters
+                                                                             ],
+                                                                    value=number_of_clusters[3]
+                                                                    ),
+                                                       html.Div(style={"padding": "20px"}, children=[
+                                                                                                     dcc.Graph(id="cluster")
+                                                                                                     ]),
+                                                       html.Div(className="text-center1", children=[
+                                                                                                    html.P(""),
+                                                                                                    html.P("The amount of variance explained is ... (need to add)"),
+                                                                                                             html.P("Would also like to add scree plot as in other shiny app example")
+                                                                                                             ]),
+                                                       html.Div(className="jumbotron text-center1", children=[
+                                                                                                              html.H1("About"),
+                                                                                                              html.P("This app is supposed to be a simple example of how to deploy a dash app. Work in progress."),
+                                                                                                              html.P("Number of Clusters - Let's take a look at an indicator of how many clusters to choose")
+                                                                                                              ]),
+                                                       html.Div(style={"padding": "50px"},children=[
+                                                                          html.H4(children='Data'),
+                                                                          generate_table(df)
+                                                                          ])
+                                                       
+                                                       ])
+
+# import external css
+app.css.append_css({"external_url": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"})
+
+# import external javascript
+app.scripts.append_script({"external_url": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"})
+
+# insert callbacks
+@app.callback(Output("cluster", "figure"), [Input('dropdown_x', 'value'),Input('dropdown_y', 'value'),Input('dropdown_k', 'value')])
+def update_graph(x, y, k):
+    data = []
+    num_of_clusters = df_list[k]['num_of_clusters']
+    for i in range(num_of_clusters):
+        results_df = df_list[k]['results_df']
+        cluster_df = results_df[results_df["predicted_classes"] == i]
+        data.append({
+                    "x": cluster_df[x],
+                    "y": cluster_df[y],
+                    "type": "scatter",
+                    "mode": "markers",
+                    "name": f"class_{i}",
+                    "marker": dict(
+                                   color = colors[i],
+                                   size = 10
+                                   )
+                    })
+    
+    layout = {
+        "hovermode": "closest",
+        "margin": {
+            "r": 10,
+            "t": 25,
+            "b": 40,
+            "l": 60
+        },
+        "title": f"Iris Dataset - {x} vs {y}",
+        "xaxis": {
+            "domain": [0, 1],
+            "title": x
+},
+    "yaxis": {
+        "domain": [0, 1],
+        "title": y
+    }
+    }
+    
+    fig = Figure(data=data, layout=layout)
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', debug=True, port=8050)
